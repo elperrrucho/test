ece4968123b02588aa2840bfa43cc3a33557e751
Elrey <eddygarcia06@gmail.com>
2020-11-22 12:57:15 -0500

    nouveau fichier

--- new.cpp
+++ new.cpp
@@ -1,4 +1,7 @@
 #Inslcude des bitch 
 je suis ecouere detudier etne [as me rendre nulle part 
 je vai sodnner mon 1000 pourcent et je vais percer
+ je vais nique ce cours ci 
+je vais me battre opour etre un as 
+je vais percer 
 
